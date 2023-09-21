dc3de3bdb9e28e3a33ed35b06c5ea7372ee1efd4
jens.jakobsson@solita.se <jens.jakobsson@solita.se>
2023-09-21 12:11:54 +0000

    Saved recipe: FPL.compute_fpldata2

--- recipes/compute_fpldata2.py
+++ recipes/compute_fpldata2.py
@@ -14,7 +14,7 @@ fpldata_df = fpldata.get_dataframe()
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
 def get_player_X_and_y(name):
-    df_player = fpldata_df.loc[df['name'] == name]
+    df_player = fpldata_df.loc[fpldata_df['name'] == name]
     df_actual_game_attributes = df_player.loc[:, ['round', 'kickoff_time', 'was_home', 'difficulty', 'opponent_team', 'playing_in', 'total_points']]
     df_actual_game_attributes.rename({'total_points': 'y'}, axis=1, inplace=True)
     df_rolling_mean_attributes = df_player.drop(['round', 'name', 'element', 'kickoff_time', 'difficulty', 'opponent_team', 'transfers_balance', 'value', 'was_home', 'playing_in'], axis=1).shift(1).rolling(3).mean()
